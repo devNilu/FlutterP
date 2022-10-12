8002608c8e2bc2c77ab52a4e90a3761fbd7c97b5
devNilu <reach.nilpatil@gmail.com>
2022-10-12 18:26:07 +0530

    Day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main(){
@@ -14,7 +14,9 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,16 +6,29 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.blue,
+      color: Colors.white,
+      child:Column(
+        children: [
+          Image.asset("assets/images/login_image.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text("Welcome",
+            style: TextStyle(
+            fontSize: 22,
             fontWeight: FontWeight.bold,
           ),
-        ),
-      ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextFormField(
+
+          )
+        ],
+      )
     );
   }
 }
