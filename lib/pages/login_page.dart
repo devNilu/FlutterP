009d480caffa7703e3a7e7322b6d70a61ab928fa
devNilu <reach.nilpatil@gmail.com>
2022-10-13 17:27:49 +0530

    Day6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name="";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +22,16 @@ class LoginPage extends StatelessWidget {
             Image.asset("assets/images/login_image.png",
               fit: BoxFit.cover,
             ),
-            SizedBox(
+             const SizedBox(
               height: 20.0,
             ),
-            Text("Welcome",
-              style: TextStyle(
+            Text("Welcome $name",
+              style: const TextStyle(
               fontSize: 22,
               fontWeight: FontWeight.bold,
             ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20.0,
             ),
             Padding(
@@ -31,27 +39,61 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                 TextFormField(
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: "Enter Username",
                     labelText: "Username",
                   ),
+                  onChanged: (value){
+                    name = value;
+                    setState((){});
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: "Enter Password",
                     labelText:"Password",
                   ),
                 ),
-                  SizedBox(
+                  const SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(child: Text("Login"),
-                  style: TextButton.styleFrom(maximumSize: Size(150, 40)),
-                  onPressed: () {
-                   Navigator.pushNamed(context, MyRoutes.homeRoute);
-    },
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    changeButton = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                child: AnimatedContainer(
+                  duration: const Duration(seconds: 1),
+                  width: changeButton? 50 : 140,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child:  changeButton
+                      ? const Icon(
+                    Icons.done,
+                    color: Colors.white,
+                  )
+                  :const Text("Login",
+                    style: TextStyle(color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
                   ),
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                     borderRadius:
+                     BorderRadius.circular(changeButton?50 : 8)),
+                ),
+              ),
+    //               ElevatedButton(child: Text("Login"),
+    //               style: TextButton.styleFrom(maximumSize: Size(150, 40)),
+    //               onPressed: () {
+    //                Navigator.pushNamed(context, MyRoutes.homeRoute);
+    // },
+    //               ),
               ],
               ),
             ),
