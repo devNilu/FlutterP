de935406fa4f2a2538660a7f4e6b7cc0ed2f9b46
devNilu <reach.nilpatil@gmail.com>
2022-10-14 15:45:32 +0530

    Day6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/pages/notification_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
@@ -26,7 +27,8 @@ class Myapp extends StatelessWidget {
       routes: {
         "/":(context)=>LoginPage(),
         MyRoutes.homeRoute:(context)=>HomePage(),
-        MyRoutes.loginRoute:(context)=>LoginPage()
+        MyRoutes.loginRoute:(context)=>LoginPage(),
+        MyRoutes.notificationRoute:(context)=>NotificationPage()
       },
     );
   }
