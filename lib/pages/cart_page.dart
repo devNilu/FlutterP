06a709e4a793664a80b4185f23042f6fbd632712
devNilu <reach.nilpatil@gmail.com>
2022-10-19 11:46:58 +0530

    Placeholder | Cart Page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -13,6 +16,62 @@ class CartPage extends StatelessWidget {
         backgroundColor: context.canvasColor,
         title: "Cart".text.make(),
       ),
+      body: Column(
+        children: [
+          Placeholder().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
     );
   }
 }
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+              onPressed: (){},
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(context.theme.buttonColor)),
+              child: "Buy".text.white.make(),
+          ).w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context,index) => ListTile(
+          leading: Icon(Icons.done),
+          trailing: IconButton(
+            icon: Icon(Icons.remove_circle_outline),
+            onPressed: (){},
+          ),
+          title: "Item".text.make(),
+        )
+    );
+  }
+}
+
